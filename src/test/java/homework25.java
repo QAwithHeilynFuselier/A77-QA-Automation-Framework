d413dc02ef0c8f857db400dc5ca914797ba8180b
TestPro-Support <115383552+testpro-support@users.noreply.github.com>
2025-10-08 20:29:48 +0800

    Update README.md 2. Create your new Lambdatest account with a free trial here: https://accounts.lambdatest.com/register.
    3. Add cloud execution capability to your framework.
    
    4. Implement parallel testing, including the usage of ThreadLocal for thread safety. You can refer to Lesson 25 slides or the live class for guidance.
    
    5. Run your tests in the cloud and ensure they pass (check the videos on LambdaTest).
    
    6. Commit your changes to the new branch 'homework-25'.

---
+++ src/test/java/homework25.java
@@ -0,0 +1,51 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+
+public class homework25 extends BaseTest {
+
+    @Test
+    public void renamePlaylist() {
+
+        String playlistName = "Heilyn";
+
+        // Login
+        WebElement emailField = getDriver().findElement(By.cssSelector("input[placeholder='Email Address']"));
+        emailField.sendKeys("heilyn.fuselier@testpro.io");
+
+        WebElement passwordField = getDriver().findElement(By.cssSelector("input[placeholder='Password']"));
+        passwordField.sendKeys("HolaMundo@2025");
+
+        getDriver().findElement(By.xpath("//button[text()='Log In']")).click();
+
+        // Open playlist
+        WebElement playlist = new WebDriverWait(getDriver(), Duration.ofSeconds(30))
+                .until(ExpectedConditions.elementToBeClickable(By.cssSelector(".playlist:nth-child(3)")));
+        playlist.click();
+
+        // Rename playlist
+        Actions actions = new Actions(getDriver());
+        actions.contextClick(playlist).perform();
+
+        WebElement editButton = new WebDriverWait(getDriver(), Duration.ofSeconds(30))
+                .until(ExpectedConditions.elementToBeClickable(By.xpath("//li[contains(text(),'Edit')]")));
+        actions.moveToElement(editButton).click().perform();
+
+        WebElement editingPlaylist = new WebDriverWait(getDriver(), Duration.ofSeconds(30))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("li.playlist.editing")));
+
+        WebElement renameInput = editingPlaylist.findElement(By.tagName("input"));
+        renameInput.sendKeys(Keys.chord(Keys.CONTROL, "a"));
+        renameInput.sendKeys(Keys.DELETE);
+        renameInput.sendKeys("Christmas");
+        renameInput.sendKeys(Keys.ENTER);
+    }
+}
